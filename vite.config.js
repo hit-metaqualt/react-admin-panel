a6abc567a73c3cac7580089619e3c1c41ff2bcaf
hit-metaqualt <hitbhuva@metaqualt.in>
2025-03-21 10:21:27 +0530

    make .env dynamic

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          [
-            "@locator/babel-jsx/dist",
-            {
-              env: "development",
-            },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            [
+              "@locator/babel-jsx/dist",
+              {
+                env: "development",
+              },
+            ],
           ],
-        ],
-      },
-    }),
-  ],
+        },
+      }),
+    ],
+    define: {
+      'process.env': env,
+    },
+  };
 });
